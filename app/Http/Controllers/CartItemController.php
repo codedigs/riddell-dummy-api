5a1930835c6cbf4707f8f3ffa4a34aa0bdb6ce20
codedigs <rodrigogalura3rd@gmail.com>
2019-11-06 13:54:18 +0800

    return status after fix changes

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -817,7 +817,7 @@ class CartItemController extends Controller
             if ($result instanceof ChangeLog)
             {
                 $cartItem = CartItem::find($cart_item_id);
-                $cartItem->markAsPendingApproval();
+                $cartItem->markAsIncomplete(false); // false means not removing approval link
 
                 return response()->json([
                     'success' => true,
