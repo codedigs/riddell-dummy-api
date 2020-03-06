9cfec5b314da97ef81896412d6432766f4679197
codedigs <rodrigogalura3rd@gmail.com>
2020-03-06 18:08:04 +0800

    optimize when getting styles

--- app/Http/Controllers/StyleController.php
+++ app/Http/Controllers/StyleController.php
@@ -3,18 +3,57 @@
 namespace App\Http\Controllers;
 
 use App\Api\Prolook\StyleApi;
+use App\Api\Qx7\GroupCutApi;
 
 class StyleController extends Controller
 {
     public function getStylesByGroupCutId($group_cut_id)
     {
-        $styleApi = new StyleApi;
-        $styles = $styleApi->getByGroupCutId($group_cut_id);
+        $styles = new \stdClass;
+        $styles->styles = [];
+        $styles->success = false;
 
-        $result = json_encode($styles);
-        $result = json_decode($result, true); // convert to array
+        $groupCutApi = new GroupCutApi;
+        $groupCut = $groupCutApi->getGroupStylesById($group_cut_id);
 
-        return response()->json($result);
+        if ($groupCut->success)
+        {
+            if (isset($groupCut->master_block_pattern_group))
+            {
+                $blockPatterns = $groupCut->master_block_pattern_group->block_patterns;
+
+                foreach ($blockPatterns as $blockPattern)
+                {
+                    if (!empty($blockPattern->skus))
+                    {
+                        $skus = $blockPattern->skus;
+
+                        foreach ($skus as $sku)
+                        {
+                            if (!in_array($sku->style_id, array_column($styles->styles, "style_id")))
+                            {
+                                array_push($styles->styles, [
+                                    'style_id' => $sku->style_id,
+                                    'info' => [
+                                        'name' => $sku->style_name,
+                                        'thumbnail_path' => $sku->thumbnail_path,
+                                        'neck_option' => $sku->neck_option
+                                    ]
+                                ]);
+                            }
+                        }
+                    }
+                }
+
+                $styles->success = true;
+                return response()->json($styles);
+            }
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => $groupCut->message
+        ]);
     }
 
     public function getStylesByCutId($cut_id)
