800ca249b8e643a333b86bfd265d9a2f0573e1c9
codedigs <rodrigogalura3rd@gmail.com>
2020-03-12 11:13:13 +0800

    fix not deleting roster properly

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -341,7 +341,7 @@ class CartItem extends Model
             return $r['category'] === static::ROSTER_CATEGORY_YOUTH;
         });
 
-        $this->roster = json_encode($newRoster);
+        $this->roster = json_encode(array_values($newRoster));
         return $this->save();
     }
 
@@ -353,7 +353,7 @@ class CartItem extends Model
             return $r['category'] === static::ROSTER_CATEGORY_ADULT;
         });
 
-        $this->roster = json_encode($newRoster);
+        $this->roster = json_encode(array_values($newRoster));
         return $this->save();
     }
 
