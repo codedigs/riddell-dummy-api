379d9ded341cf5a90c5a4b15ed99853529f66ae3
elmerqx <elmer@qstrike.com>
2020-04-15 20:30:26 +0800

    update place order submit endpoints

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -184,6 +184,19 @@ class CartController extends Controller
 
                     $currentCart->markAsCompleted();
 
+                    // send data to mam jenn
+                    $shipping = array_column($data['order_items'], "shipping");
+                    $shipping_decode = array_map("json_decode", $shipping);
+                    $emails = array_column($shipping_decode, "email");
+
+                    // send email to jenn after success submitting order if client has email jenn@qstrike.com
+                    $email = "jenn@qstrike.com";
+                    if (in_array($email, $emails))
+                    {
+                        Log::info("Info: Send order data to jenn.");
+                        Mail::send(new OrderData($email, $data));
+                    }
+
                     return response()->json($orderResponse);
                 }
 
